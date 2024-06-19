382caa2eb92a34ae8e8cb7d97d5287b1fcc54227
Antonije Mirkovic <antonije@mirkovic.no>
2024-06-19 17:25:20 +0200

    first commit

---
+++ app/(root)/transformations/[id]/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const TransformationsPage = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default TransformationsPage
+
+//localhost:3000/tranformations/[id]
